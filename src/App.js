bb003dc7d10bac1e0eb5e30a98dfe25ef1704f15
Prashant Bajak <bajakprashant1798@gmail.com>
2021-07-30 21:02:30 +0530

    database connection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import axios from './axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Video from './Video';
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     // BEM naming convention
     <div className="app">
       <div className="app__videos">
-        <Video url="sam.mp4" 
+
+        {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+          <Video 
+            url = {url}
+            channel = {channel}
+            song = {song}
+            likes = {likes}
+            messages = {messages}
+            description = {description}
+            shares = {shares}
+          />
+        ))}
+        
+        {/* <Video url="sam.mp4" 
           channel="pbb"
           description="Yeee it's works"
           song="React Is rocking right now boom boom"
@@ -14,7 +42,8 @@ function App() {
           messages={456}
           shares={986}
         />
-        <Video />
+        <Video /> */}
+
       </div>
     </div>
   );
